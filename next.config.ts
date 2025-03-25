e68c43e6526fd2a5d91ef4bd8dec43898c966271
Zach Forrester <zach.forrester@gmail.com>
2025-03-25 14:09:08 -0400

    setup SCSS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    implementation: "sass-embedded",
+  },
 };
 
 export default nextConfig;
